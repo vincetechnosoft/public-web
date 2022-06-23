cd87390a2ad3d488a1e5490ee52f7be07bc7798b
Panth <hybrid977@gmail.com>
2022-06-23 23:38:36 +0530

    download button added

--- src/components/solutionInfo.tsx
+++ src/components/solutionInfo.tsx
@@ -1,3 +1,8 @@
+import Button from "./button";
+import { Download } from "react-feather";
+import { useCallback, useState } from "react";
+import { AndroudIcon } from "./icons";
+
 interface Props {
   id: string;
   title: string;
@@ -34,7 +39,38 @@ const SolutionInfo: React.FC<Props> = ({
           <li key={x}>{x}</li>
         ))}
       </ul>
+      <div className="my-10 flex justify-end">
+        <DownloadAndroidApk id={id} />
+      </div>
     </div>
   );
 };
+
 export default SolutionInfo;
+
+let getUrl: ((id: string) => Promise<string>) | undefined;
+
+function DownloadAndroidApk({ id }: { id: string }) {
+  const [loading, setLoading] = useState(false);
+  const onClick = useCallback(
+    async function () {
+      setLoading(true);
+      try {
+        getUrl ??= (await import("data/firebase")).default;
+        const url = await getUrl(id);
+        window.open(url);
+      } catch {}
+      setLoading(false);
+    },
+    [id]
+  );
+  return (
+    <Button
+      onClick={onClick}
+      loading={loading}
+      leadingIcon={<AndroudIcon className="mr-3" />}
+    >
+      Download Apk
+    </Button>
+  );
+}
